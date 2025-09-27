2dbc4a6e45621b66dd35586ced1a77cacf66a19b
Valentamayog <vatamayog@unal.edu.co>
2025-09-27 08:48:46 -0500

    Solution PRE-06

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -9,7 +9,7 @@ from sklearn.metrics import mean_squared_error
 def test_01():
     """Test if the model is able to predict the MPG of a car."""
 
-    dataset = pd.read_csv("auto_mpg.csv")
+    dataset = pd.read_csv("files/input/auto_mpg.csv")
     dataset = dataset.dropna()
     dataset["Origin"] = dataset["Origin"].map(
         {1: "USA", 2: "Europe", 3: "Japan"},
@@ -17,10 +17,10 @@ def test_01():
     dataset = pd.get_dummies(dataset, columns=["Origin"], prefix="", prefix_sep="")
     y_true = dataset.pop("MPG")
 
-    with open("mlp.pickle", "rb") as file:
+    with open("homework/mlp.pickle", "rb") as file:
         mlp = pickle.load(file)
 
-    with open("features_scaler.pickle", "rb") as file:
+    with open("homework/features_scaler.pickle", "rb") as file:
         features_scaler = pickle.load(file)
 
     standarized_dataset = features_scaler.transform(dataset)
